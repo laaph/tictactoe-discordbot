cd4857fa4ab812975bb049af67f6a45f13db4888
laaph <laaph@laaph.com>
2020-07-23 13:54:40 +0300

    updated tests

--- test/test_tictactoe.py
+++ test/test_tictactoe.py
@@ -4,19 +4,25 @@ import random  # because let's test things at random
 
 import tictactoe
 
+
+# Because we do not have a setup.py, you will have to either set the
+# sys.path variable (via PYTHONPATH) or python -m pytest test/test_tictactoe.py
+
 def test_tictactoe():
-	tic = tictactoe.TicTacToe()
-	assert tic.board[random.randint(0, 8)] is None
-	assert tic.player_turn == 0
-	assert type(tic.player_names) == list
-	assert type(tic.board) == list
+    tic = tictactoe.TicTacToe()
+    assert tic.board[random.randint(0, 8)] is None
+    assert tic.player_turn == 0
+    assert type(tic.player_names) == list
+    assert type(tic.board) == list
+
 
 def test_taketurnandverifyturnchange():
-	tic = tictactoe.TicTacToe()
-	tic.new_game("player1", "player2")
-	tic.take_ai_turn()
+    tic = tictactoe.TicTacToe()
+    tic.new_game("player1", "player2")
+    tic.take_turn(1, 0)
 
-	assert tic.player_turn == 2
+    assert tic.board[0] == 1
+    assert tic.player_turn == 2
 
 # @pytest.patch('random')
 # def test_turn(random):
